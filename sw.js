2951a5ed7ef38ae339ac1914f1d46a4eedb1fdbc
Ian <ian@Ians-MacBook-Pro.local>
2019-05-12 01:58:38 +0900

    Fix

--- sw.js
+++ sw.js
@@ -15,8 +15,8 @@ self.addEventListener('install', function(e) {
             // added to the cache.
             return cache.addAll([
                 '/',
-                '/index.html',
-                '/manifest.json',
+                '/jpy2twd/index.html',
+                '/jpy2twd/manifest.json',
                 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css',
                 'https://code.jquery.com/jquery-3.3.1.slim.min.js',
                 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js',
